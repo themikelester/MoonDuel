d2f9ac22071112939c7358ef5c12fb64e7061a3b
Mike Lester <themikelester@gmail.com>
2020-02-14 16:18:21 +1100

    Added webpack-glsl-minify to load .glsl shaders

--- webpack.common.js
+++ webpack.common.js
@@ -28,8 +28,16 @@ module.exports = {
         },
       },
       {
-        test: /\.glsl$/,
-        loader: 'raw-loader',
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'webpack-glsl-minify',
+          options: {
+            preserveDefines: false,
+            preserveUniforms: true,
+            preserveVariables: false,
+          }
+        }
       },
     ],
   },
