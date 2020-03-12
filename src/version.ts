203201b7192a8b05a71058aa418ee9f166992fa9
Mike Lester <themikelester@gmail.com>
2020-03-13 00:20:59 +1100

    Modify the source code link to show the source tree at the revision instead of just the last commit

--- src/version.ts
+++ src/version.ts
@@ -5,7 +5,7 @@ export const GIT_SHORT_REVISION = GIT_REVISION.slice(0, 8);
 
 //@ts-ignore
 export const GITHUB_URL = __GITHUB_URL;
-export const GITHUB_REVISION_URL = `${GITHUB_URL}/commit/${GIT_REVISION}`;
+export const GITHUB_REVISION_URL = `${GITHUB_URL}/tree/${GIT_REVISION}`;
 
 //@ts-ignore
 export const IS_DEVELOPMENT: boolean = process.env.NODE_ENV === 'development';
