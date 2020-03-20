d80fef423a98df85056897d660d29f26c14be656
Mike Lester <themikelester@gmail.com>
2020-03-20 16:27:17 +1100

    Avatar loads the entire node hierarchy from GLTF

--- src/Avatar.ts
+++ src/Avatar.ts
@@ -5,22 +5,24 @@ import * as Gfx from './gfx/GfxTypes';
 import { renderLists } from "./RenderList";
 import { GlobalUniforms } from "./GlobalUniforms";
 
-import vert_source from './shaders/skinned.vert';
+import skinnedVertSource from './shaders/skinned.vert';
+import simpleVertSource from './shaders/simple.vert';
 import frag_source from './shaders/simple.frag';
 import { UniformBuffer, computePackedBufferLayout } from "./UniformBuffer";
-import { vec4, vec3, mat4, quat } from "gl-matrix";
-import { defaultValue, assert, assertDefined } from "./util";
+import { vec4, vec3, quat } from "gl-matrix";
+import { defaultValue, assert, assertDefined, defined } from "./util";
 import { Skin, Skeleton } from "./Skeleton";
+import { Object3D } from "./Object3D";
 
-const kAvatarBoneCount = 19;
+const kMaxAvatarBoneCount = 32;
 
 class AvatarShader implements Gfx.ShaderDescriptor {
-    private static vert = vert_source;
+    private static vert = skinnedVertSource;
     private static frag = frag_source;
     
     public static uniformLayout: Gfx.BufferLayout = computePackedBufferLayout({
         u_color: { type: Gfx.Type.Float4 },
-        u_bones: { type: Gfx.Type.Float4x4, count: kAvatarBoneCount },
+        u_bones: { type: Gfx.Type.Float4x4, count: kMaxAvatarBoneCount },
     });
 
     public static resourceLayout = {
@@ -29,22 +31,55 @@ class AvatarShader implements Gfx.ShaderDescriptor {
     };
 
     name = 'AvatarShader';
-    defines = `#define k_MaxBones ${kAvatarBoneCount}\n`;
+    defines = `#define k_MaxBones ${kMaxAvatarBoneCount}\n`;
     vertSource = [this.defines, AvatarShader.vert.sourceCode];
     fragSource = AvatarShader.frag.sourceCode;
     resourceLayout = AvatarShader.resourceLayout;
     id: Gfx.Id;
 }
+
+class ModelShader implements Gfx.ShaderDescriptor {
+    private static vert = simpleVertSource;
+    private static frag = frag_source;
+    
+    public static uniformLayout: Gfx.BufferLayout = computePackedBufferLayout({
+        u_color: { type: Gfx.Type.Float4 },
+    });
+
+    public static resourceLayout = {
+        uniforms: { index: 0, type: Gfx.BindingType.UniformBuffer, layout: AvatarShader.uniformLayout },
+        globalUniforms: { index: 1, type: Gfx.BindingType.UniformBuffer, layout: GlobalUniforms.bufferLayout },
+    };
+
+    name = 'ModelShader';
+    vertSource = ModelShader.vert.sourceCode;
+    fragSource = ModelShader.frag.sourceCode;
+    resourceLayout = ModelShader.resourceLayout;
+    id: Gfx.Id;
+}
+
 export class AvatarManager {
+    gfxDevice: Gfx.Renderer;
+    globalUniforms: GlobalUniforms;
+
     shader: Gfx.Id;
+    modelShader: Gfx.Id;
+
     materialUniforms: UniformBuffer;
-    models: SkinnedModel[] = [];
+    models: Model[] = [];
+    skinnedModels: SkinnedModel[] = [];
     skin: Skin;
 
+    skins: Skin[];
+    rootNodes: Object3D[];
     animations: GltfAnimation[] = [];
 
     initialize({ gfxDevice, resources, globalUniforms }: { gfxDevice: Gfx.Renderer, resources: ResourceManager, globalUniforms: GlobalUniforms }) {
+        this.gfxDevice = gfxDevice;
+        this.globalUniforms = globalUniforms;
+        
         this.shader = gfxDevice.createShader(new AvatarShader());
+        this.modelShader = gfxDevice.createShader(new ModelShader());
 
         // @TODO: UniformBuffer should support x instances 
         this.materialUniforms = new UniformBuffer('AvatarMaterial', gfxDevice, AvatarShader.uniformLayout);
@@ -52,40 +87,112 @@ export class AvatarManager {
         this.materialUniforms.write(gfxDevice);
         
         resources.load('data/CesiumMan.glb', 'gltf', (error, resource) => {
-            if (error) { console.error(`Failed to load resource`, error); }
-            else {
-                const gltf = resource as GltfResource;
+            if (error) { return console.error(`Failed to load resource`, error); }
+            
+            const gltf = resource as GltfResource;
 
-                // @HACK:
-                this.animations = gltf.animations;
+            // @HACK:
+            this.animations = gltf.animations;
 
-                // Parse skeleton
-                const skin = assertDefined((gltf.skins.length > 0) ? Skin.fromGltf(gltf, 0) : undefined);
-                this.skin = skin;
-                assert(skin.bones.length === kAvatarBoneCount);
+            // Parse skeleton(s)
+            // @TODO: This in the GLTF loader
+            this.skins = gltf.skins.map((gltfSkin, idx) => {
+                const skin = Skin.fromGltf(gltf, idx);
+                assert(skin.bones.length <= kMaxAvatarBoneCount);
+                return skin;
+            });
 
-                for (let gltfMesh of gltf.meshes) {
-                    for (let prim of gltfMesh.primitives) {
-                        const mesh = new Mesh({
-                            vertexLayout: prim.vertexLayout,
-                            vertexBuffers: prim.vertexBuffers.map(buf => buf.id),
-                            elementCount: prim.elementCount,
-                            indexBuffer: prim.indexBuffer ? prim.indexBuffer.id : undefined,
-                            indexType: prim.indexType,
-                            primitiveType: prim.type,
-                        });
-
-                        const material = new Material(gfxDevice, this.shader);
-                        const model = new SkinnedModel(gfxDevice, renderLists.opaque, mesh, material);
-                        model.bindSkeleton(new Skeleton(skin), skin.inverseBindMatrices);
-                        
-                        model.material.setUniformBuffer(gfxDevice, 'uniforms', this.materialUniforms.getBuffer());
-                        model.material.setUniformBuffer(gfxDevice, 'globalUniforms', globalUniforms.buffer);
-                        this.models.push(model);
-                    }
-                }
-            }
+            this.rootNodes = gltf.rootNodeIds.map(nodeId => this.loadNode(gltf, nodeId));
+            this.rootNodes.forEach(node => {
+                vec3.scale(node.scale, node.scale, 10.0); 
+                node.updateMatrix();
+                node.updateMatrixWorld(false, true);
+            });
         });
+    }
+
+    loadSkinnedModel(gltf: GltfResource, meshId: number, skinId: number): Object3D {
+        const gltfMesh = gltf.meshes[meshId];
+        const skin = this.skins[skinId];
+        const obj = new Object3D();
+
+        for (let prim of gltfMesh.primitives) {
+            const mesh = new Mesh({
+                vertexLayout: prim.vertexLayout,
+                vertexBuffers: prim.vertexBuffers.map(buf => buf.id),
+                elementCount: prim.elementCount,
+                indexBuffer: prim.indexBuffer ? prim.indexBuffer.id : undefined,
+                indexType: prim.indexType,
+                primitiveType: prim.type,
+            });
+
+            const material = new Material(this.gfxDevice, this.shader);
+            const model = new SkinnedModel(this.gfxDevice, renderLists.opaque, mesh, material);
+            model.bindSkeleton(new Skeleton(skin), skin.inverseBindMatrices);
+            model.material.setUniformBuffer(this.gfxDevice, 'uniforms', this.materialUniforms.getBuffer());
+            model.material.setUniformBuffer(this.gfxDevice, 'globalUniforms', this.globalUniforms.buffer);
+            this.skinnedModels.push(model);
+
+            obj.add(model);
+        }
+
+        return obj;
+    }
+
+    loadModel(gltf: GltfResource, meshId: number): Object3D {
+        const gltfMesh = gltf.meshes[meshId];
+        const obj = new Object3D();
+
+        for (let prim of gltfMesh.primitives) {
+            const mesh = new Mesh({
+                vertexLayout: prim.vertexLayout,
+                vertexBuffers: prim.vertexBuffers.map(buf => buf.id),
+                elementCount: prim.elementCount,
+                indexBuffer: prim.indexBuffer ? prim.indexBuffer.id : undefined,
+                indexType: prim.indexType,
+                primitiveType: prim.type,
+            });
+
+            const material = new Material(this.gfxDevice, this.modelShader);
+            const model = new Model(this.gfxDevice, renderLists.opaque, mesh, material);
+            model.material.setUniformBuffer(this.gfxDevice, 'uniforms', this.materialUniforms.getBuffer());
+            model.material.setUniformBuffer(this.gfxDevice, 'globalUniforms', this.globalUniforms.buffer);
+            this.models.push(model);
+
+            obj.add(model);
+        }
+
+        return obj;
+    }
+
+    loadNode(gltf: GltfResource, nodeId: number): Object3D {
+        const node = gltf.nodes[nodeId];
+        let obj: Object3D;
+
+        if (defined(node.skinId)) {
+            const meshId = assertDefined(node.meshId);
+            obj = this.loadSkinnedModel(gltf, meshId, node.skinId);
+        } else if (node.meshId) {
+            obj = this.loadModel(gltf, node.meshId);
+        } else {
+            obj = new Object3D();
+        }
+
+        obj.name = defaultValue(node.name, `Node${nodeId}`);
+        vec3.copy(obj.position, node.translation);
+        quat.copy(obj.rotation, node.rotation);
+        vec3.copy(obj.scale, node.scale);
+        obj.updateMatrix();
+
+        if (node.children) {
+            for (const childId of node.children) {
+                const childObj = this.loadNode(gltf, childId);
+                childObj.parent = obj;
+                obj.add(childObj);
+            }
+        }
+
+        return obj;
     }
 
     update({ realTime }: { realTime: number }) {
@@ -95,32 +202,30 @@ export class AvatarManager {
     
             for (let i = 0; i < anim.rotations.length; i++) {
                 const data = anim.rotations[i];
-                const bone = assertDefined(this.models[0].skeleton.bones.find(b => b.nodeId === data.nodeId));
+                const bone = assertDefined(this.skinnedModels[0].skeleton.bones.find(b => b.nodeId === data.nodeId));
                 evalRotation(t, data, bone.rotation);
             }
     
             for (let i = 0; i < anim.translations.length; i++) {
                 const data = anim.translations[i];
-                const bone = assertDefined(this.models[0].skeleton.bones.find(b => b.nodeId === data.nodeId));
+                const bone = assertDefined(this.skinnedModels[0].skeleton.bones.find(b => b.nodeId === data.nodeId));
                 evalTranslation(t, data, bone.position);
             }
     
             for (let i = 0; i < anim.scales.length; i++) {
                 const data = anim.scales[i];
-                const bone = assertDefined(this.models[0].skeleton.bones.find(b => b.nodeId === data.nodeId));
+                const bone = assertDefined(this.skinnedModels[0].skeleton.bones.find(b => b.nodeId === data.nodeId));
                 evalTranslation(t, data, bone.scale);
             }
         }
     }
 
     render({ gfxDevice }: { gfxDevice: Gfx.Renderer }) {
-        for (let i = 0; i < this.models.length; i++) {
-            const model = this.models[i];
+        for (let i = 0; i < this.skinnedModels.length; i++) {
+            const model = this.skinnedModels[i];
 
-            // const headJoint = assertDefined(model.skeleton.bones.find(b => b.name === 'Skeleton_arm_joint_L__4_'));
-            // headJoint.rotation = quat.rotateX(headJoint.rotation, headJoint.rotation, Math.PI * 0.01);
-
-            model.skeleton.evaluate();
+            model.updateMatrixWorld(true, true);
+            model.skeleton.evaluate(model.matrixWorld);
 
             const boneFloats = this.materialUniforms.getFloatArray('u_bones');
             model.skeleton.writeToBuffer(boneFloats);
