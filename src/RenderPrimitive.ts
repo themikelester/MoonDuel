d649159c7b8f127bc8d0dee6502b2cf010ea6df5
Mike Lester <themikelester@gmail.com>
2020-03-30 15:34:26 +1100

    RenderPrimitive is now a class so that it can have a constructor for convenience

--- src/RenderPrimitive.ts
+++ src/RenderPrimitive.ts
@@ -1,6 +1,6 @@
 import { Id, BufferView, PrimitiveType, Type, CullMode } from './gfx/GfxTypes';
 
-export interface RenderPrimitive {
+export interface IRenderPrimitive {
     resourceTable: Id;
     vertexTable: Id;
     renderPipeline: Id;
@@ -12,4 +12,27 @@ export interface RenderPrimitive {
 
     depthMode?: Id;
     cullMode?: CullMode;
+}
+
+export class RenderPrimitive implements IRenderPrimitive {
+    resourceTable: Id;
+    vertexTable: Id;
+    renderPipeline: Id;
+    elementCount: number;
+    type: PrimitiveType;
+
+    indexBuffer?: BufferView;
+    indexType?: Type
+
+    depthMode?: Id;
+    cullMode?: CullMode;
+
+    constructor(renderPipeline: Id, vertexTable: Id, resourceTable: Id) {
+        this.resourceTable = resourceTable;
+        this.vertexTable = vertexTable; 
+        this.renderPipeline = renderPipeline;
+
+        this.elementCount = 0;
+        this.type = PrimitiveType.Triangles;
+    }
 }
