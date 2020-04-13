253caaa39aecb54300f5bcd06207421a2f465351
Mike Lester <themikelester@gmail.com>
2020-04-13 18:17:49 +1000

    Added NetClient and NetPacket
    One level up from WebUDP. Same as Client and Packet on the server. Handles reliability (keeps track of which packets have been acknowledged by the server). Tested and working.

---
+++ src/NetClient.ts
@@ -0,0 +1,98 @@
+
+import { WebUdpSocket, WebUdpEvent } from './WebUdp';
+import { sequenceNumberGreaterThan, sequenceNumberWrap, PacketBuffer } from './NetPacket';
+import { EventDispatcher } from './EventDispatcher';
+
+export enum NetClientEvent {
+    Connect = "connect",
+    Receive = "receive",
+    Acknowledge = "acknowledge",
+};
+
+const kPacketHistoryLength = 32;
+
+/**
+ * High level class controlling communication with the server. Handles packet reliability and buffering.
+ * @NOTE: This needs to be kept in sync with its counterpart on the server side, Client.cpp/h
+ */
+export class NetClient extends EventDispatcher {
+    private socket: WebUdpSocket;
+
+    private remoteSequence = 0;
+    private localSequence = 0;
+    private localHistory: PacketBuffer = new PacketBuffer(kPacketHistoryLength);
+    private remoteHistory: PacketBuffer = new PacketBuffer(kPacketHistoryLength);
+
+    initialize(serverAddress: string) {
+        this.socket = new WebUdpSocket(serverAddress);
+        this.socket.on(WebUdpEvent.Open, this.onOpen.bind(this));
+        this.socket.on(WebUdpEvent.Message, this.onMessage.bind(this));
+        this.socket.connect();
+    }
+
+    private onOpen(evt: WebUdpEvent) {
+        console.log('Connected to server');
+        this.fire(NetClientEvent.Connect);
+    }
+
+    private onMessage(evt: MessageEvent) {
+        const data = evt.data;
+        if (data instanceof ArrayBuffer) {
+            this.receive(data);
+        } else {
+            console.log("received:", data);
+        }
+    }
+
+    private readAckBitfield(bitfield: number, sequence: number, history: PacketBuffer) {
+        for (const packet of history) {
+            const bit = sequenceNumberWrap(sequence - packet.sequence);
+            const ackd = bitfield & (1 << bit);
+            if (ackd && !packet.isAcknowledged()) {
+                packet.setAcknowledged();
+                this.fire(NetClientEvent.Acknowledge, packet.gamePacket);
+            }
+        }
+        return bitfield;
+    }
+
+    private writeAckBitfield(sequence: number, history: PacketBuffer) {
+        let bitfield = 0;
+        for (const packet of history) {
+            const bit = sequenceNumberWrap(sequence - packet.sequence);
+            if (bit < 32) {
+                bitfield |= 1 << bit;
+            }
+        }
+        return bitfield;
+    }
+
+    receive(data: ArrayBuffer) {
+        const packet = this.remoteHistory.allocate();
+        packet.fromBuffer(data);
+
+        // If this packet is newer than the latest packet we've received, update
+        if (sequenceNumberGreaterThan(packet.sequence, this.remoteSequence)) {
+            this.remoteSequence = packet.sequence;
+        }
+
+        // Update the acknowledged state of all of the recently sent packets
+        this.readAckBitfield(packet.ackBitfield, packet.ack, this.localHistory);
+
+        this.fire(NetClientEvent.Receive, packet.gamePacket);
+    }
+
+    send(data: Uint8Array) {
+        const packet = this.localHistory.allocate();
+
+        packet.sequence = this.localSequence;
+        packet.ack = this.remoteSequence;
+        packet.ackBitfield = this.writeAckBitfield(this.remoteSequence, this.remoteHistory);
+        packet.gamePacket = data;
+        const bytes = packet.toBuffer();
+
+        this.socket.send(bytes);
+
+        this.localSequence += 1;
+    }
+}
---
+++ src/NetPacket.ts
@@ -0,0 +1,150 @@
+import { assert } from "./util";
+
+export type SequenceNumber = number;
+
+const kPacketHeaderSize = 8;
+const kMaxGamePacketSize = 1024;
+
+export class Packet {
+    sequence: SequenceNumber;
+    ack: SequenceNumber;
+    ackBitfield: number;
+    gamePacket: Uint8Array;
+
+    private readonly dataView: DataView;
+    private readonly bytes: Uint8Array;
+    private _acknowledged = false;
+
+    constructor(bytes: Uint8Array) {
+        this.bytes = bytes;
+        this.dataView = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength);
+    }
+
+    onAllocate() {
+        this._acknowledged = false;
+        this.sequence = -1;
+        this.ack = -1;
+        this.ackBitfield = 0;
+    }
+
+    fromBuffer(buffer: ArrayBuffer) {
+        this.bytes.set(new Uint8Array(buffer));
+
+        this.sequence = this.dataView.getUint16(0, true);
+        this.ack = this.dataView.getUint16(2, true);
+        this.ackBitfield = this.dataView.getUint32(4, true);
+        this.gamePacket = this.bytes.subarray(kPacketHeaderSize, buffer.byteLength);
+
+        return this;
+    }
+
+    toBuffer(): Uint8Array {
+        this.dataView.setUint16(0, this.sequence, true);
+        this.dataView.setUint16(2, this.ack, true);
+        this.dataView.setUint32(4, this.ackBitfield, true);
+        this.bytes.set(this.gamePacket, kPacketHeaderSize);
+
+        return this.bytes.subarray(0, kPacketHeaderSize + this.gamePacket.byteLength);
+    }
+
+    isAcknowledged() { 
+        return this._acknowledged;
+    }
+
+    setAcknowledged() {
+        this._acknowledged = true;
+    }
+}
+
+export class PacketBuffer implements Iterable<Packet> {
+    private packets: Packet[];
+    private buffer: ArrayBuffer;
+    private readonly capacity: number;
+
+    private writeHead: number = 0;
+    private readHead: number = 0;
+    private full: boolean = false;
+
+    constructor(capacity: number) {
+        this.capacity = capacity;
+
+        const packetBufferSize = (kPacketHeaderSize + kMaxGamePacketSize);
+        this.buffer = new ArrayBuffer(packetBufferSize * capacity);
+
+        this.packets = [];
+        for (let i = 0; i < capacity; i++) {
+            this.packets[i] = new Packet(new Uint8Array(this.buffer, i * packetBufferSize, packetBufferSize));
+        }
+    }
+
+    // To support for..of loops
+    [Symbol.iterator](): Iterator<Packet> {
+        class PacketBufferIterator implements Iterator<Packet> {
+            index: number = 0;
+            count: number;
+
+            constructor(private buffer: PacketBuffer) {
+                this.count = buffer.count();
+            }
+
+            next(): IteratorResult<Packet, any> {
+                if (this.index >= this.count) { return { value: null, done: true }; }
+                
+                const i = (this.buffer.readHead + this.index) % this.buffer.capacity;
+                this.index += 1;
+                return { value: this.buffer.packets[i] };
+            }
+        }
+
+        return new PacketBufferIterator(this);
+    }
+
+    allocate(): Packet {
+        const packet = this.packets[this.writeHead];
+        packet.onAllocate();
+
+        if (this.full) {
+            this.readHead = (this.readHead + 1) % this.capacity;
+        }
+        this.writeHead = (this.writeHead + 1) % this.capacity;
+        this.full = this.writeHead === this.readHead;
+
+        return packet;
+    }
+
+    get(index: number) {
+        assert(!this.isEmpty());
+        const i = (this.readHead + index) % this.capacity;
+        return this.packets[i];
+    }
+
+    isEmpty() {
+        return (!this.full && (this.readHead == this.writeHead));
+    }
+
+    isFull() {
+        return this.full;
+    }
+
+    count() {
+        let count = this.capacity;
+        if (!this.full) {
+            if (this.writeHead >= this.readHead) {
+                count = this.writeHead - this.readHead;
+            }
+            else {
+                count = this.capacity + this.writeHead - this.readHead;
+            }
+        }
+        return count;
+    }
+}
+
+export function sequenceNumberGreaterThan(a: SequenceNumber, b: SequenceNumber) {
+    return ((a > b) && (a - b <= 32768)) ||
+        ((a < b) && (b - a > 32768));
+}
+
+export function sequenceNumberWrap(a: SequenceNumber) {
+    return a % 65536;
+}
